993dce82028f7e582324f1117491bcd2d9fd313e
Subhash Chand <subhashverma3@gmail.com>
2020-03-15 00:41:50 +0530

    End of secutity chapter 3

---
+++ DatingApp.API/Data/AuthRepository.cs
@@ -0,0 +1,80 @@
+﻿using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Unicode;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext context;
+
+        public AuthRepository(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public DataContext Context { get; }
+
+        public async Task<User> Login(string userName, string passwrod)
+        {
+            var user = await context.Users.FirstOrDefaultAsync(u => u.UserName == userName);
+            if(user == null)
+            {
+                return null;
+            }
+
+            if(!VerifyPasswordHash(passwrod,user.PasswordHash, user.PasswordSalt))
+            {
+                return null;
+            }
+            return user;
+        }
+
+        private bool VerifyPasswordHash(string passwrod, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {             
+               var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(passwrod));
+                for(int i=0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] == passwordHash[i]) return false;
+                }
+                return true;
+            }
+        }
+
+        public async Task<User> Register(User user, string password)
+        {
+            byte[] passwordHash, passwordSalt;
+            CreatePasswordHash(password, out passwordHash, out passwordSalt);
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+            await context.Users.AddAsync(user);
+            await context.SaveChangesAsync();
+            return user;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using(var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+            
+        }
+
+        public async Task<bool> UserExists(string username)
+        {
+            if (await context.Users.AnyAsync(x => x.UserName == username))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ DatingApp.API/Data/IAuthRepository.cs
@@ -0,0 +1,14 @@
+﻿using DatingApp.API.Models;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Data
+{
+    public interface IAuthRepository
+    {
+        Task<User> Register(User user, string passowrd);
+        
+        Task<User> Login(string username, string passwrod);
+
+        Task<bool> UserExists(string username);
+    }
+}
